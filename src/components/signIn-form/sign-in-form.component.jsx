480d722806171b6182f0b7e61f81af92b1231bf6
Asma-Abed <abed.asma@hotmail.com>
2022-10-04 15:15:42 +0100

    observer added, on auth changed

--- src/components/signIn-form/sign-in-form.component.jsx
+++ src/components/signIn-form/sign-in-form.component.jsx
@@ -1,9 +1,7 @@
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import Button from '../btn/btn.component';
-
-import { UserContext } from '../../contexts/user.context';
 
 import {
   signInWithGooglePopup,
@@ -22,15 +20,12 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
-  const { setCurrentUser } = useContext(UserContext);
-
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocFromAuth(user);
+    await signInWithGooglePopup();
   };
 
   const handleSubmit = async (event) => {
@@ -38,7 +33,7 @@ const SignInForm = () => {
 
     try {
       const { user } = await SignInUserWithEmailPassword(email, password);
-      setCurrentUser(user);
+
       resetFormFields();
     } catch (err) {
       switch (err.code) {
