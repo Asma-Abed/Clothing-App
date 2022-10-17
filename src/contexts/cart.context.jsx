5a8308beb0c160a0a231f36c41a86ad7a1c45267
Asma-Abed <abed.asma@hotmail.com>
2022-10-17 13:28:18 +0100

    add to cart functionality implemented

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,36 @@
 import { createContext, useState } from 'react';
 
+const addCartItem = (cartItems, productToAdd) => {
+  // 1. find if cartitems contain product to add
+  const exsistingCartITem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  // 2. if found, increment quantity
+  if (exsistingCartITem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // 3. return new array with modifeid cartitems/ new cart item
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
