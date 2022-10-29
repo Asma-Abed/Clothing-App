473f89b4afdf3361647a441ff480184a6db7d9fb
Asma-Abed <abed.asma@hotmail.com>
2022-10-29 18:44:34 +0100

    adding collection to firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,12 +1,18 @@
-import { useState, createContext } from 'react';
-import PRODUCTS from '../shop-data.json';
+import { useState, createContext, useEffect } from 'react';
+
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
+import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    addCollectionAndDocuments('categories', SHOP_DATA);
+  }, []);
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
